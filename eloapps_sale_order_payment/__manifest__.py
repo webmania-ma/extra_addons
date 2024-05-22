ebfd3c489842c25a4a3d2965c63b5b149e954315
Driss El mouedden <d.elmouedden@gmail.com>
2024-05-22 21:59:57 +0100

    add stock_move_invoice

--- eloapps_sale_order_payment/__manifest__.py
+++ eloapps_sale_order_payment/__manifest__.py
@@ -7,7 +7,7 @@
     'version': "14.0.3.1",
     'category': 'Sales/sales',
 
-
+    
     "contributors": [
         "1 <Nassim REFES>",
         "2 <Ould Miloud Mohamed>",
@@ -28,7 +28,7 @@
     'sequence': 1,
 
 
-
+   
     'depends': ['sale_management','stock','sale_stock'],
 
 
@@ -38,13 +38,16 @@
         'reports/action_stock_picking_report.xml',
         'reports/stock_picking.xml',
         'reports/sale_report.xml',
+
         'wizard/sale_order_payment.xml',
+
         'views/account_journal.xml',
         'views/account_payment.xml',
         'views/sale_order.xml',
         'views/account_move_line.xml',
         'views/stock_picking.xml',
         'views/add_print_button.xml',
+
         'wizard/sale_payment.xml',
     ],
 
